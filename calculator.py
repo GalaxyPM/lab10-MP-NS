cc0e84efef0c0ec5f5d29bf2d50bce59a89fce07
GalaxyPM <palmermarcelo24@gmail.com>
2025-04-10 15:38:38 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -18,8 +18,6 @@ def hypotenuse(a, b):
 def add(a, b):
     return a + b
 
-def sub(a, b):
-    return  a - b
 def mul(a, b):
     return a * b
 def div(a, b):
@@ -27,10 +25,6 @@ def div(a, b):
         raise ZeroDivisionError
     return b / a
 
-def log(a, b):
-    if b<=0 or a<=1:
-        raise ValueError
-    return math.log(b,a)
 def exp(a, b):
     return a**b
 
@@ -41,18 +35,10 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-def multiply(a, b):
-    return a * b
-
-
-
 def logarithm(a, b):
     if b <= 0 or a <= 1:
         raise ValueError
     return math.log(b,a)# use math library/raise ValueError
-
-def exponent(a, b):
-    return a**b
 
 
 
